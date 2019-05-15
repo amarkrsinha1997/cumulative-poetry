4c30d0956f6d36e085872fc207e8e67abe488d1d
amarkrsinha1997 <amar17@navgurukul.org>
2019-05-15 18:42:11 +0530

    Added revealForDay functionality
    - command line args for recite & revealed added
    - revealForDay functionality Added
    - No CLI args exception added

--- src/poet.py
+++ src/poet.py
@@ -1,5 +1,11 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--reveal-for-day', nargs=1, help="", dest="revealDay", type=int)
+parser.add_argument('--recite', action="store_true", help="")
+
+
 class Poetry:
-    messages = [
+    tales = [
         'the house that Jack built.',
         'the malth that lay in',
         'the rat that ate',
@@ -14,11 +20,43 @@ class Poetry:
         'the horse and the hound and the horn that belonged to'
     ]
 
-
     def __init__(self):
         pass
 
     def revealForDay(self, revealDay=1):
         # initiated message
-        taleRevealed = "This is "
-        
+        talesRevealed = "This is "
+
+        if revealDay > len(self.tales):
+            return "Invalid Day"
+
+        talesToBeRevealed = self.tales[:revealDay]
+
+        talesToBeRevealed.reverse()
+
+        for tale in talesToBeRevealed:
+            talesRevealed += "{0}\n\t".format(tale)
+
+        return talesRevealed
+
+
+if __name__ == "__main__":
+    poetry = Poetry()
+    args = parser.parse_args()
+    args = vars(args)
+    
+    shouldRecite = args['recite']
+    revealDayArg = args['revealDay']
+
+
+    if not shouldRecite and not revealDayArg:
+        parser.print_help()
+        print("\n")
+        raise Exception("Need either of the args.")
+
+    if shouldRecite:
+        pass
+    else:
+        revealDay = revealDayArg[0]
+        tale = poetry.revealForDay(revealDay)
+        print(tale)
