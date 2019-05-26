888f3fbd91c057f51177335dcc97b639ba272d90
amarkrsinha1997 <amar17@navgurukul.org>
2019-05-26 14:29:01 +0530

    Added Todo's for tests.

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ from poet import Poet
 from poem import Poem, POEM
 
 
-class poetTest(unittest.TestCase):
+class poetTest(unittest.TestCase): # TODO - class name is supposed to be capital 
     def test_reveal_for_day_1(self):
         poet = Poet(Poem(POEM))
         actualTale = poet.revealForDay(1)
@@ -11,7 +11,7 @@ class poetTest(unittest.TestCase):
 
         self.assertEqual(actualTale, expectedTale)
 
-    def test_reveal_for_day_5(self):
+    def test_reveal_for_day_5(self): # TODO - try to see if you can  use multi-line strings
         poet = Poet(Poem(POEM))
         actualTale = poet.revealForDay(5)
 
@@ -23,16 +23,16 @@ class poetTest(unittest.TestCase):
 
         self.assertEqual(actualTale, expectedTale)        
 
-    def test_recite_contains_day(self):
+    def test_recite_contains_day(self): # TODO - hard to understand, what is Tale Day? The initial problem has no such word.
         poet = Poet(Poem(POEM))
         tale = poet.recite()
 
-        actualTaleDay = len(poet.poem.getPoem())
+        actualTaleDay = len(poet.poem.getPoem()) # TODO - don't invent words
         expectedTaleDay = tale.count("Day")
         self.assertEqual(actualTaleDay, expectedTaleDay)
         
 
-    def test_recite_reveal_everyday_tale(self):
+    def test_recite_reveal_everyday_tale(self): # TODO - the test can be more specific, like how many days should exist? What should be the length of the poem? 
         poet = Poet(Poem(POEM))
         tale = poet.recite()
 
@@ -41,7 +41,7 @@ class poetTest(unittest.TestCase):
         
         for day in range(1, actualTaleDay + 1):
             dayString = "Day {0} -".format(day)
-            self.assertNotEqual(tale.index(dayString), -1)
+            self.assertNotEqual(tale.index(dayString), -1) # TODO Complicated way of saying that something should exist. Is there is a simpler method to do that.
 
 if __name__ == "__main__":
     unittest.main()
