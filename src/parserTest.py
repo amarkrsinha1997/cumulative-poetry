3ed2ca6c91b33cc077456f58e4177c97c8708ea2
amarkrsinha1997 <amar17@navgurukul.org>
2019-05-26 15:09:25 +0530

    Remoed commented lines

--- src/parserTest.py
+++ src/parserTest.py
@@ -9,8 +9,5 @@ class ParserTest(unittest.TestCase):
     @mock.patch('argparse.ArgumentParser.parse_args',
             return_value=argparse.Namespace(shouldRecite=True, forWhichDay=2))
     def test_only_arguments_provided(self, mock_parser):
-        # mock_parser.parse_args.return_value = {"shouldRecite": True, "forWhichDay": 2}
         parser = Parser()
-        print(parser.parser)
-        # import pdb; pdb.set_trace()
         self.assertRaises(OnlyOneArgsExeception, parser.checkArgs)
