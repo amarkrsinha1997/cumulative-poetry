3500a5815661532774848523538144d68fc538d8
amarkrsinha1997 <amar17@navgurukul.org>
2019-05-26 15:23:27 +0530

    Added tests for
    - when the parser should recite
    - when the parser shouldn't recite

--- src/parserTest.py
+++ src/parserTest.py
@@ -11,3 +11,18 @@ class ParserTest(unittest.TestCase):
     def test_only_arguments_provided(self, mock_parser):
         parser = Parser()
         self.assertRaises(OnlyOneArgsExeception, parser.checkArgs)
+
+    @mock.patch('argparse.ArgumentParser.parse_args',
+            return_value=argparse.Namespace(shouldRecite=False, forWhichDay=[2]))
+    def test_should_not_recite(self, mock_parser):
+        parser = Parser()
+        parser.checkArgs()
+        self.assertEqual(parser.getArgs(), {"shouldRecite":False, "forWhichDay": [2]})
+
+    @mock.patch('argparse.ArgumentParser.parse_args',
+            return_value=argparse.Namespace(shouldRecite=True, forWhichDay=None))
+    def test_should_recite(self, mock_parser):
+        parser = Parser()
+        parser.checkArgs()
+        self.assertEqual(parser.getArgs(), {"shouldRecite":True, "forWhichDay": None})
+
