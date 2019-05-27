9a14282e467b78f5e97aa79097fcc6721e42589b
amarkrsinha1997 <amar17@navgurukul.org>
2019-05-27 19:53:15 +0530

    renamed plugins to formatters

--- src/echoFormatters.py
+++ src/echoFormatters.py
@@ -1,20 +1,20 @@
 # seperate what varies from what stays the same.
 
-class Echo:
+class EchoFormatter:
     def echo(self, array):
         return ["{0}\n\t{1}".format(elem, elem) for elem in array]
 
 
-class NoEcho:
+class NoEchoFormatter:
     def echo(self, array):
         return array
 
 ## OCP - open - closed principle - open for extension, closed for modification...
 
-class AlternatingEcho:
+class AlternatingEchoFormatter:
     def _echo(self,i,line):
         if i % 2 == 0:
-            return Echo().echo([line])[0]
+            return EchoFormatter().echo([line])[0]
         else:
             return line
     
